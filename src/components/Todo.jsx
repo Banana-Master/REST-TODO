2fd1527186c0c450ad52613c4e668c8b02fa7292
Banana-Master <tjdgh0313@gmail.com>
2023-08-30 20:11:07 +0900

    [추가] Todo간단한 스타일 설정

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { styled } from 'styled-components' 
 
 function Todo() {
+
+  const TodoBox = styled.div`
+    border: 3px solid green;
+    border-radius: 3px;
+    margin: 1rem;
+    padding: 1rem;
+    width: 300px;
+  `
+
+  const ButtonBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  `
+
+  const Button = styled.button`
+    width: 120px;
+    height: 35px;
+    background-color: white;
+    border-radius: 5px;
+    cursor: pointer;
+    border: none;
+    font-weight: 600;
+  `
+
+
   return (
-    <div>Todo</div>
+    <>
+      <TodoBox>
+        <h3>제목입니다.</h3>
+        <p>내용입니다.</p>
+        <ButtonBox>
+          <Button style={{backgroundColor: '#FDE5EC'}}>삭제하기</Button>
+          <Button style={{backgroundColor: '#CECE5A'}}>완료</Button>
+        </ButtonBox>
+      </TodoBox>
+    </>
   )
 }
 
