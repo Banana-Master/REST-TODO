95dbe1d4bbd5b9c317742d6488c617282e32b402
Banana-Master <tjdgh0313@gmail.com>
2023-08-30 20:11:31 +0900

    [수정] 컴포넌트 배치

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import Todo from '../components/Todo'
+import { styled } from 'styled-components'
+import Todos from '../components/Todos'
+import InputForm from '../components/InputForm'
 
 function Home() {
+
+  const Body = styled.div`
+    max-width: 1200px;
+    min-width: 800px;
+    margin-left: auto;
+    margin-right: auto;
+    background-color: beige;
+  `
+  const Title = styled.div`
+    padding: 1rem 2rem;
+    display: flex;
+    justify-content: space-between;
+
+  `
+
+
   return (
-    <div>Home</div>
+    <Body>
+      <Title>
+        <span>My Todo List</span>
+        <span>React</span>
+      </Title>
+      <InputForm></InputForm>
+
+      <Todos>
+        
+      </Todos>
+      <Todo></Todo>
+    </Body>
   )
 }
 
